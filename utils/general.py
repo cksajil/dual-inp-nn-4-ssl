0b463550f7068aefe99a5cd7c9e74db596bcf287
cksajil <sajilck@gmail.com>
2023-09-13 13:59:43 +0530

    Added function to compute STFT

--- utils/general.py
+++ utils/general.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import stft
 
 
 def generate_white_noise(mu=0, std=1, nsamples=1000):
@@ -8,3 +9,8 @@ def generate_white_noise(mu=0, std=1, nsamples=1000):
     """
     x = np.random.normal(mu, std, size=nsamples)
     return x
+
+
+def compute_stft(x, Fs, N=1000):
+    f, t, Zxx = stft(x, Fs, nperseg=N)
+    return f, t, Zxx
