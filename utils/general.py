8ce56e4f30bfa59343713d3d579b95effe3e5673
cksajil <sajilck@gmail.com>
2023-09-13 14:01:24 +0530

    Added STFT compute function

--- utils/general.py
+++ utils/general.py
@@ -12,5 +12,9 @@ def generate_white_noise(mu=0, std=1, nsamples=1000):
 
 
 def compute_stft(x, Fs, N=1000):
+    """
+    Wrapper Function to compute Short Time Fourier
+    Transform using Scipy library
+    """
     f, t, Zxx = stft(x, Fs, nperseg=N)
     return f, t, Zxx
